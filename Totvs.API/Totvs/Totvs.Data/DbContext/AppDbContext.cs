7da681f0ddab2f84a0e477ff622bc7f944111f4d
Interfile_CRibeiro <interfile_cribeiro@br.atento.com>
2025-11-21 08:15:14 -0300

    novas melhorias na API

---
+++ Totvs.API/Totvs/Totvs.Data/DbContext/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Totvs.Domain.Entities;
+
+namespace Totvs.Data.DbContext;
+
+public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Seed opcional
+        modelBuilder.Entity<Product>().HasData(
+            new Product { Id = 1, Name = "Notebook", Price = 4500m, StockQuantity = 5, Active = true },
+            new Product { Id = 2, Name = "Mouse Gamer", Price = 120m, StockQuantity = 20, Active = true }
+        );
+    }
+}
---
+++ Totvs.API/Totvs/Totvs.Domain/Interfaces/Repository/Base/IBaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Totvs.Domain.Interfaces.Repository.Base;
+
+public interface IBaseRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(int id);
+    Task<List<T>> GetAllAsync();
+
+    Task<List<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+
+    Task<bool> ExistsAsync(int id);
+}
